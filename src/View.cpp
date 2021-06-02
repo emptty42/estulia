301231aa03132a5ff0cb21572cfe99c9abe410c5
Martin Quinson <martin.quinson@ens-rennes.fr>
2021-06-02 18:56:52 +0200

    Try with computeTextureCoords

--- src/View.cpp
+++ src/View.cpp
@@ -9,8 +9,10 @@ View::View(std::shared_ptr<World> world, gf::Window* window): world_(world),
     gf::Texture texture("data/carrier_walk.png");
     texture.setSmooth();
     for (int anim = 0; anim<6;anim++)
-        for (unsigned frame = 0; frame < 10; ++frame)
-            animation_[anim].addFrame(texture, gf::RectF::fromPositionSize({ 0.1f * frame, 1.f / 6.f * anim }, { 0.25f, 0.5f }), gf::milliseconds(100));
+        for (int frame = 0; frame < 10; ++frame) {
+            auto coord = texture.computeTextureCoords(gf::RectI::fromMinMax(gf::Vector<int,2>({frame*25,anim*252/6}), {(frame+1)*25,(anim+1)*252/6}));
+            animation_[anim].addFrame(texture, coord, gf::milliseconds(100));
+        }
 
     renderer_->clear(gf::Color::White);
 
